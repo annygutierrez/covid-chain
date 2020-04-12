2bcfa0328c609eaa9936f245795aff1f11ba0364
annygutierrez16 <agutilop@everis.com>
2020-04-11 22:09:51 -0500

    Add proff of work method
    - Add proff of work difficulty.

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,12 +1,15 @@
 const SHA256 = require('crypto-js/sha256');
 
+const DIFFICULTY = 4;
+
 class Block {
     // Stablish values for a block
-    constructor(timeStamp, lastHash, hash, data) {
+    constructor(timeStamp, lastHash, hash, data, nonce) {
        this.timeStamp = timeStamp;
        this.lastHash = lastHash;
        this.hash = hash;
        this.data = data;
+       this.nonce = nonce;
     }
 
     // To debug
@@ -15,27 +18,34 @@ class Block {
         timestamp: ${this.timeStamp}
         lastHash: ${this.lastHash.substring(0, 10)}
         hash: ${this.hash.substring(0, 10)}
+        nonce: ${this.nonce}
         data: ${this.data}
         `
     }
 
     // This meyhod creates a genessis block
     static genesis() {
-        return new this('Genesis time', '--------', 'f1r2t-h45h', []);
+        return new this('Genesis time', '--------', 'f1r2t-h45h', [], 0);
     }
 
 
     // To mine a block - Create a new one - add a new block to the chain
     static mineBlock(lastBlock, data) {
-       const timestamp = Date.now();
+       let timestamp, hash;
        const lastHash = lastBlock.hash;
-       const hash = Block.hash(timestamp, lastHash, data);
+       let nonce = 0;
 
-       return new this(timestamp, lastHash, hash, data);
+       do {
+        nonce++;
+        timestamp = Date.now();
+        hash = Block.hash(timestamp, lastHash, data, nonce);
+       } while (hash.substring(0, DIFFICULTY) !== '0'.repeat(DIFFICULTY));
+
+       return new this(timestamp, lastHash, hash, data, nonce);
     }
 
-    static hash(timestamp, lastHash, data) {
-        return SHA256(`${timestamp}${lastHash}${data}`).toString();
+    static hash(timestamp, lastHash, data, nonce) {
+        return SHA256(`${timestamp}${lastHash}${data}${nonce}`).toString();
     }
 
     static blockHash(block) {
